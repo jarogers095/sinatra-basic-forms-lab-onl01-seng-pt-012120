1a6e76be867a76cb3dbd8c15ee6a720c9b5e0b93
James Rogers <jarogers095@gmail.com>
2020-04-16 06:22:54 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -3,7 +3,7 @@ class Puppy
   attr_reader :breed
   attr_accessor :name, :age
   
-  def initialize(name: nil, breed: nil, age: nil)
+  def initialize(name: "dog", breed: "breed", age: 0)
     @name = name
     @breed = breed
     @age = age
